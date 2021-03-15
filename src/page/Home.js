c3f9c8129742b5540b5a2a3bf32f74dd02564797
peter3403 <56294368+peter3403@users.noreply.github.com>
2021-03-15 10:42:47 +0800

    Update Home.js

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,12 +1,14 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import ProductList from "../components/ProductList";
+import Navbar from "../components/Navbar"
 
 function Home() {
   return (
     <div className="bg-color">
         <div className="container grid-container">
             <Header />
+            <Navbar />
             <ProductList />
             <Footer />
         </div>
